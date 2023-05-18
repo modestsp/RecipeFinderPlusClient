7193a386d881a88da08704ccf6cd1bac2a061484
srsebbi <csperichon@gmail.com>
2023-05-18 20:38:18 -0300

    Added display for each recipe

--- src/App.tsx
+++ src/App.tsx
@@ -5,43 +5,46 @@ import styles from "./styles/App.module.css";
 
 function App() {
   const [current, setCurrent] = useState<string>("");
-  const [words, setWords] = useState<string[]>([]);
+  const [params, setParams] = useState<string[]>([]);
+  const [recipes, setRecipes] = useState<any[]>([]);
 
   const handleSubmit = () => {
-    const params = words.join(",");
+    const paramsToSearch = params.join(",");
     fetch(
-      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${params}&number=2&apiKey=${
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${paramsToSearch}&number=2&apiKey=${
         import.meta.env.VITE_API_KEY
       }`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setRecipes(data));
     console.log(
-      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${params}&number=2&apiKey=${
+      `${import.meta.env.VITE_INGREDIENTS_ROUTE}${params}&number=2&apiKey=${
         import.meta.env.VITE_API_KEY
       }`
     );
   };
-
   return (
     <div className={styles.mainContainer}>
-      <input
-        name="word"
-        value={current}
-        onChange={(e) => setCurrent(e.target.value)}
-        type="text"
-        placeholder="Search"
-      />
-      <button onClick={() => setWords([...words, current])}>Add</button>
       <h1>RecipeFinderPlus</h1>
       <p>Welcome to the recipe finder</p>
-      <ul>
-        {words.map((word, index) => {
-          console.log("aca la word", words[index]);
+      <div>
+        <input
+          name="word"
+          value={current}
+          onChange={(e) => setCurrent(e.target.value)}
+          type="text"
+          placeholder="Search"
+        />
+        <button onClick={() => setParams([...params, current])}>Add</button>
+      </div>
+      <ul className={styles.paramsList}>
+        {params.map((param, index) => {
           return (
-            <li key={index}>
-              <p>{word}</p>
-              <button onClick={() => setWords(words.filter((w) => w !== word))}>
+            <li key={index} className={styles.paramsListItem}>
+              <p>{param}</p>
+              <button
+                onClick={() => setParams(params.filter((w) => w !== param))}
+              >
                 delete
               </button>
             </li>
@@ -49,6 +52,16 @@ function App() {
         })}
       </ul>
       <button onClick={handleSubmit}>Search</button>
+      <div>
+        {recipes.map((recipe) => {
+          return (
+            <div key={recipe.id}>
+              <h1>{recipe.title}</h1>
+              <img src={recipe.image} alt={recipe.title} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
