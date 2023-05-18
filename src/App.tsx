e4a9791dd1e80305268da7aec6bb33a5c928c2f4
srsebbi <csperichon@gmail.com>
2023-05-17 21:10:44 -0300

    Added functionality for displays search params, and search the recipe

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,51 @@ import viteLogo from "/vite.svg";
 import styles from "./styles/App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [current, setCurrent] = useState<string>("");
+  const [words, setWords] = useState<string[]>([]);
+
+  const handleSubmit = () => {
+    const params = words.join(",");
+    fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${params}&number=2&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+    console.log(
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${params}&number=2&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
+  };
 
   return (
     <div className={styles.mainContainer}>
+      <input
+        name="word"
+        value={current}
+        onChange={(e) => setCurrent(e.target.value)}
+        type="text"
+        placeholder="Search"
+      />
+      <button onClick={() => setWords([...words, current])}>Add</button>
       <h1>RecipeFinderPlus</h1>
       <p>Welcome to the recipe finder</p>
-      <p>Seach</p>
-      <button>search</button>
+      <ul>
+        {words.map((word, index) => {
+          console.log("aca la word", words[index]);
+          return (
+            <li key={index}>
+              <p>{word}</p>
+              <button onClick={() => setWords(words.filter((w) => w !== word))}>
+                delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={handleSubmit}>Search</button>
     </div>
   );
 }
