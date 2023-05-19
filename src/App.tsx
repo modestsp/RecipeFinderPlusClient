ad3a03a882fad62b3bf1012da5c329ad69752abf
srsebbi <csperichon@gmail.com>
2023-05-19 19:45:14 -0300

    Added async away, errorMsg and classnames

--- src/App.tsx
+++ src/App.tsx
@@ -2,60 +2,74 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import styles from "./styles/App.module.css";
+import { GrAdd } from "react-icons/gr";
 
 function App() {
   const [current, setCurrent] = useState<string>("");
   const [params, setParams] = useState<string[]>([]);
   const [recipes, setRecipes] = useState<any[]>([]);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const paramsToSearch = params.join(",");
-    fetch(
+    const result = await fetch(
       `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${paramsToSearch}&number=2&apiKey=${
         import.meta.env.VITE_API_KEY
       }`
-    )
-      .then((res) => res.json())
-      .then((data) => setRecipes(data));
+    );
+    const recipes = await result.json();
+    if (recipes.length == 0) setErrorMsg("No recipes found");
+    else setErrorMsg(null);
+    setRecipes(recipes);
+
     console.log(
       `${import.meta.env.VITE_INGREDIENTS_ROUTE}${params}&number=2&apiKey=${
         import.meta.env.VITE_API_KEY
       }`
     );
   };
+
   return (
     <div className={styles.mainContainer}>
       <h1>RecipeFinderPlus</h1>
       <p>Welcome to the recipe finder</p>
-      <div>
+      <div className={styles.addParamContainer}>
         <input
           name="word"
           value={current}
           onChange={(e) => setCurrent(e.target.value)}
           type="text"
-          placeholder="Search"
+          placeholder="Add a ingredient"
         />
-        <button onClick={() => setParams([...params, current])}>Add</button>
+        <button
+          onClick={() => setParams([...params, current])}
+          className={styles.addButton}
+        >
+          +
+        </button>
       </div>
       <ul className={styles.paramsList}>
         {params.map((param, index) => {
           return (
-            <li key={index} className={styles.paramsListItem}>
-              <p>{param}</p>
-              <button
-                onClick={() => setParams(params.filter((w) => w !== param))}
-              >
-                delete
-              </button>
+            <li
+              key={index}
+              className={styles.paramsListItem}
+              onClick={() => setParams(params.filter((w) => w !== param))}
+            >
+              <span className={styles.removeParamButton}>X</span>
+              <p className={styles.param}>{param}</p>
             </li>
           );
         })}
       </ul>
-      <button onClick={handleSubmit}>Search</button>
-      <div>
+      <button className={styles.searchButton} onClick={handleSubmit}>
+        Search
+      </button>
+      <div className={styles.recipesContainer}>
+        {errorMsg && <p>{errorMsg}</p>}
         {recipes.map((recipe) => {
           return (
-            <div key={recipe.id}>
+            <div className={styles.recipe} key={recipe.id}>
               <h1>{recipe.title}</h1>
               <img src={recipe.image} alt={recipe.title} />
             </div>
