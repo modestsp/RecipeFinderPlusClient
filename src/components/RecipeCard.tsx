c35d3c5497f92618ddfbb46ee15d6f17266af567
srsebbi <csperichon@gmail.com>
2023-05-24 20:40:47 -0300

    Added redirect on click for recipes

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,19 +1,26 @@
 import styles from "../styles/RecipeCard.module.css";
 import { IRecipe } from "../utils/types";
+import { useNavigate } from "react-router-dom";
 
 interface RecipeCardProps {
   recipe: IRecipe;
 }
 
 const RecipeCard = ({ recipe }: RecipeCardProps) => {
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.container}>
+    <div
+      onClick={() => navigate(`/recipes/${recipe.id}`)}
+      className={styles.container}
+    >
       <h1 className={styles.recipeTitle}>{recipe.title}</h1>
       <img
         className={styles.recipeImage}
         src={recipe.image}
         alt={recipe.title}
       />
+      <span>likes: {recipe.likes}</span>
     </div>
   );
 };
