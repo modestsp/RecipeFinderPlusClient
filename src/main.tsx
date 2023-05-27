2a92ebb9324fe45d3591751643674d4fa19e2141
srsebbi <csperichon@gmail.com>
2023-05-26 22:15:27 -0300

    added recipe route

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import Layout from './components/Layout/Layout.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import Layout from "./components/Layout/Layout.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Recipe from "./pages/Recipe.tsx";
+import Trending from "./pages/Trending.tsx";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <App />
+      </Layout>
+    ),
+    // children: [
+    //   {
+    //     path: "/recipes/:recipeId",
+    //     element: <Recipe />,
+    //   },
+    // ],
+  },
+  {
+    path: "recipes/:recipeId",
+    element: (
+      <Layout>
+        <Recipe />
+      </Layout>
+    ),
+  },
+  {
+    path: "trending",
+    element: (
+      <Layout>
+        <Trending />
+      </Layout>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Layout>
-      <App />
-    </Layout>
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
