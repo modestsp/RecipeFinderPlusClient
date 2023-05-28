9bdf23649d7e49ab2464d58c4ee2d07171a92c34
srsebbi <csperichon@gmail.com>
2023-05-28 19:44:04 -0300

    Added types for recipe ingredient

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -9,3 +9,11 @@ export interface IRecipeStep{
     number: number
     step: string
 }
+
+export interface IRecipeIngredient{
+    name: string
+    nameClean: string
+    original: string
+    originalName: string
+    unit: string
+}
