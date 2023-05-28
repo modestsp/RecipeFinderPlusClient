11b1a5338236722b52808583d35c059c547da812
srsebbi <csperichon@gmail.com>
2023-05-28 19:43:18 -0300

    Remove comments and added recipe ingredients section

--- src/pages/Recipe.tsx
+++ src/pages/Recipe.tsx
@@ -1,8 +1,9 @@
+import styles from "../styles/RecipePage.module.css";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useSWR from "swr";
 import { fetcher } from "../utils/fetcher";
-import { IRecipeStep } from "../utils/types";
+import { IRecipeIngredient, IRecipeStep } from "../utils/types";
 import { useRecipesStore } from "../utils/context";
 import { useQuery } from "react-query";
 
@@ -23,18 +24,6 @@ const Recipe = () => {
     ).then((res) => res.json())
   );
 
-  // const { isLoading, error, data } = useQuery({
-  //   queryKey: ["recipeDetails"],
-  //   queryFn: () => {
-  //     fetch(
-  //       `${import.meta.env.VITE_RECIPES_ROUTE}/${recipeId}/information?apiKey=${
-  //         import.meta.env.VITE_API_KEY
-  //       }&includeNutrition=false`
-  //     ).then((res) => res.json());
-  //   },
-  //   refetchInterval: 10000000,
-  // });
-
   if (recipeDetails.isLoading || recipeInstructions.isLoading)
     return <div>Loading...</div>;
   if (recipeDetails.error || recipeInstructions.error) return <div>Error</div>;
@@ -44,14 +33,37 @@ const Recipe = () => {
 
   return (
     <div>
-      <h1>{recipeDetails.data.title}</h1>
-      <img src={recipeDetails.data.image} alt={recipeDetails.data.title} />
-      {steps.map((step: IRecipeStep) => (
-        <div key={step.number}>
-          <h2>Step {step.number}</h2>
-          <p>{step.step}</p>
-        </div>
-      ))}
+      <div className={styles.recipeInfoContainer}>
+        <h1 className={styles.recipeTitle}>{recipeDetails.data.title}</h1>
+        <h2 className={styles.recipeReadyInMinutes}>
+          {recipeDetails.data.readyInMinutes}'
+        </h2>
+        {/* <p>{recipeDetails.data.instructions}</p> */}
+        {/* <p>{recipeDetails.data.analyzedInstructions[0].steps[0].step}</p> */}
+        <img
+          className={styles.recipeImage}
+          src={recipeDetails.data.image}
+          alt={recipeDetails.data.title}
+        />
+      </div>
+      <ul className={styles.ingredientsList}>Ingredients</ul>
+      {recipeDetails.data.extendedIngredients.map(
+        (ingredient: IRecipeIngredient) => (
+          <p>{ingredient.original}</p>
+        )
+      )}
+      <div className={styles.stepsContainer}>
+        <h3>Instructions</h3>
+        {recipeDetails.data.analyzedInstructions[0].steps.map(
+          (step: IRecipeStep) => (
+            <div className={styles.stepContainer} key={step.number}>
+              <h2>Step {step.number}</h2>
+              <p>{step.step}</p>
+            </div>
+          )
+        )}
+      </div>
+      <p>{`Rate this Recipe! :) HandsUp or HandsDown`}</p>
     </div>
   );
 };
