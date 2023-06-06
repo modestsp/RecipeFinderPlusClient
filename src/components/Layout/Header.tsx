b5a4d5b5d8ebdaa8a877b3d6be8224c31eccbaa7
srsebbi <csperichon@gmail.com>
2023-06-05 22:12:58 -0300

    Added links to trending and home pages

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,12 +1,17 @@
 import styles from "../../styles/Layout.module.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <p>LOGO</p>
       <ul>
-        <li>Trending</li>
-        <li>Home</li>
+        <li>
+          <Link to={"/trending"}>Trending</Link>
+        </li>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
         <li>Git</li>
       </ul>
     </header>
