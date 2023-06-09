a953f6271fe085f7a5f94123492102e3395b330e
srsebbi <csperichon@gmail.com>
2023-06-09 19:22:48 -0300

    Update trending page for displaying current trending recipes

--- src/pages/Trending.tsx
+++ src/pages/Trending.tsx
@@ -1,7 +1,22 @@
+import { useQuery } from "react-query";
+import { IRecipe } from "../utils/types";
+
 const Trending = () => {
+  const { data, isLoading, isError } = useQuery("trendingRecipes", () =>
+    fetch(`https://localhost:7222/recipes/trending`).then((res) => res.json())
+  );
+  if (isLoading) return <div>Loading...</div>;
+  console.log(data);
   return (
     <div>
       <h1>TRENDING RECIPES</h1>
+      {data.map((recipe: IRecipe) => (
+        <div key={recipe.id}>
+          <h2>{recipe.title}</h2>
+          <img src={recipe.image} alt={recipe.title} />
+          <p>likes: {recipe.likes}</p>
+        </div>
+      ))}
     </div>
   );
 };
