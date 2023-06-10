09595db9dc96523d1750031d60807270d741daa6
srsebbi <csperichon@gmail.com>
2023-06-10 20:19:14 -0300

    Added recipeCard for displaying recipes

--- src/pages/Trending.tsx
+++ src/pages/Trending.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "react-query";
 import { IRecipe } from "../utils/types";
-
+import RecipeCard from "../components/RecipeCard";
+import styles from "../styles/Trending.module.css";
 const Trending = () => {
   const { data, isLoading, isError } = useQuery("trendingRecipes", () =>
     fetch(`https://localhost:7222/recipes/trending`).then((res) => res.json())
@@ -8,14 +9,15 @@ const Trending = () => {
   if (isLoading) return <div>Loading...</div>;
   console.log(data);
   return (
-    <div>
+    <div className={styles.mainContainer}>
       <h1>TRENDING RECIPES</h1>
       {data.map((recipe: IRecipe) => (
-        <div key={recipe.id}>
-          <h2>{recipe.title}</h2>
-          <img src={recipe.image} alt={recipe.title} />
-          <p>likes: {recipe.likes}</p>
-        </div>
+        <RecipeCard recipe={recipe} key={recipe.id} />
+        // <div key={recipe.id}>
+        //   <h2>{recipe.title}</h2>
+        //   <img src={recipe.image} alt={recipe.title} />
+        //   <p>likes: {recipe.likes}</p>
+        // </div>
       ))}
     </div>
   );
