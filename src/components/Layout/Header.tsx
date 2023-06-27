40e8e0897339f6892ab1b5854c6318ec999fc20f
modestsp <csperichon@gmail.com>
2023-06-26 22:03:37 -0300

    Added selected tab functionality

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,8 +1,19 @@
 import styles from "../../styles/Layout.module.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import githubIcon from "../../assets/githubIcon.svg";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [tabSelected, setTabSelected] = useState<string | null>(null);
+  const location = useLocation();
+  useEffect(() => {
+    if (location.pathname === "/") {
+      setTabSelected("Home");
+    } else if (location.pathname === "/trending") {
+      setTabSelected("Trending");
+    }
+  });
+  console.log(location.pathname);
   return (
     <header className={styles.header}>
       <p className={styles.logo}>
@@ -15,12 +26,23 @@ const Header = () => {
           </Link>
         </li>
         <li>
-          <Link className={styles.link} to={"/"}>
+          <Link
+            style={{
+              textDecoration: tabSelected === "Home" ? "underline" : "",
+            }}
+            className={styles.link}
+            to={"/"}
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link to="https://github.com/modestsp">
+          <Link
+            style={{
+              textDecoration: tabSelected === "Trending" ? "underline" : "",
+            }}
+            to="https://github.com/modestsp"
+          >
             <img src={githubIcon} alt="github" />
           </Link>
         </li>
