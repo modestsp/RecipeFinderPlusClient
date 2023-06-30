f8d9df1259719dd327fe88b4a40037557d20e7dc
modestsp <csperichon@gmail.com>
2023-06-29 21:12:44 -0300

    Updated with tailwind and shadcn-ui

--- src/pages/Recipe.tsx
+++ src/pages/Recipe.tsx
@@ -2,7 +2,15 @@ import { useParams } from "react-router-dom";
 import { IRecipeIngredient, IRecipeStep } from "../utils/types";
 import { useMutation, useQuery } from "react-query";
 import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card";
+import { Car } from "lucide-react";
 const Recipe = () => {
   const { recipeId } = useParams();
   const recipeDetails = useQuery("repoData", () =>
@@ -16,7 +24,7 @@ const Recipe = () => {
   const mutation = useMutation({
     mutationFn: (data: any) => {
       return fetch("https://localhost:7222/recipes", {
-        method: "POST",
+        method: `POST`,
         headers: {
           "Content-Type": "application/json",
         },
@@ -39,36 +47,46 @@ const Recipe = () => {
   };
 
   return (
-    <div>
-      <div>
-        <h1>{recipeDetails.data.title}</h1>
-        <h2>{recipeDetails.data.readyInMinutes}'</h2>
+    <Card>
+      <CardHeader>
+        <CardTitle>{recipeDetails.data.title}</CardTitle>
+        {/* <CardDescription>
+              Ready in {recipeDetails.data.readyInMinutes}'
+            </CardDescription> */}
+      </CardHeader>
+      <CardContent>
         <img src={recipeDetails.data.image} alt={recipeDetails.data.title} />
-      </div>
-      <h3>Ingredients</h3>
-      <ul>
-        {recipeDetails.data.extendedIngredients.map(
-          (ingredient: IRecipeIngredient) => (
-            <p key={ingredient.original}>{ingredient.original}</p>
-          )
-        )}
-      </ul>
-      <div>
-        <h3>Instructions</h3>
-        {recipeDetails.data.analyzedInstructions[0].steps.map(
-          (step: IRecipeStep) => (
-            <div key={step.number}>
-              <h2>Step {step.number}</h2>
-              <p>{step.step}</p>
-            </div>
-          )
-        )}
-      </div>
-      <p style={{ fontWeight: "bold" }}>{`Leave a like if you like it! :) `}</p>
-      <button onClick={handleAddRecipe}>
-        {mutation.isLoading ? "Loading..." : <AiOutlineLike />}
-      </button>
-    </div>
+
+        <p className="text-3xl font-bold mt-2 mb-2">Ingredients</p>
+        <ul className="list-disc pl-7 bg-slate-300 rounded-sm shadow-lg p-2">
+          {recipeDetails.data.extendedIngredients.map(
+            (ingredient: IRecipeIngredient) => (
+              <li key={ingredient.original}>{ingredient.original}</li>
+            )
+          )}
+        </ul>
+        <div>
+          <p className="text-3xl font-bold mt-2 mb-2">Instructions</p>
+          {recipeDetails.data.analyzedInstructions[0].steps.map(
+            (step: IRecipeStep) => (
+              <ul
+                className="list-disc pl-4 shadow bg-gray-400 mb-2 rounded-sm py-1"
+                key={step.number}
+              >
+                <p className="text-xl font-semibold ">Step {step.number}</p>
+                <p>{step.step}</p>
+              </ul>
+            )
+          )}
+        </div>
+        <p
+          style={{ fontWeight: "bold" }}
+        >{`Leave a like if you like it! :) `}</p>
+        <button onClick={handleAddRecipe}>
+          {mutation.isLoading ? "Loading..." : <AiOutlineLike />}
+        </button>
+      </CardContent>
+    </Card>
   );
 };
 
